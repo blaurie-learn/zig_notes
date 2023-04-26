3ee4fa0acfeda8f8e864d2f17d7875143e9ac2fa
Brad laurie <blaurie@users.noreply.github.com>
2023-04-25 22:46:21 -0600

    to inline switch

--- src/main.zig
+++ src/main.zig
@@ -1391,3 +1391,112 @@ test "empty blocks" {
     try expect(@TypeOf(a) == void);
     try expect(@TypeOf(b) == void);
 }
+
+test "simple Switch" {
+    const a: u64 = 10;
+    const zz: u64 = 103;
+
+    //all branches of a switch must be coerced to a common type
+    //branches cannot fall though, if you need fallthough, combine the cases
+    //and use an if
+
+    const b = switch (a) {
+        //multiple cases combined with a comma
+        1, 2, 3 => 0,
+
+        //ranges can be specified with the ... syntax. Inclusive both ends
+        5...100 => 1,
+
+        //branches can be arbitrarily complex
+        101 => blk: {
+            const c: u64 = 5;
+            break :blk c * 2 + 1;
+        },
+
+        //switching on arbitrary expressions is allowed as long as the
+        //expression is known at compile time
+        zz => zz,
+        blk: {
+            const d: u32 = 5;
+            const e: u32 = 100;
+            break :blk d + e;
+        } => 107,
+
+        //The else branch catches everything not already captured
+        //else is mandatory unless the entire range of values is handled
+        else => 9,
+    };
+
+    try expect(b == 1);
+}
+
+//switch can be outside of a function
+const os_msg = switch (builtin.target.os.tag) {
+    .linux => "we found a linux user",
+    else => "not a linux user",
+};
+
+//inside a function, switch statements are implcitly compile time evaluated
+//if the target expression is compile time known
+test "switch inside funcion" {
+    switch (builtin.target.os.tag) {
+        .fuchsia => {
+            //On an OS other than fuchsia, block is not even analyzed,
+            //so this compiler error is not triggered.
+            //on fuchsia, this compile error would be triggered
+            @compileError("fuchsia not supported");
+        },
+        else => {},
+    }
+}
+
+test "switch tagged union" {
+    const Pt = struct {
+        x: u8,
+        y: u8,
+    };
+    const Item = union(enum) {
+        a: u32,
+        c: Pt,
+        d,
+        e: u32,
+    };
+
+    var a = Item{ .c = Pt{ .x = 1, .y = 2 } };
+
+    //switching on more complex enums is allowed
+    const b = switch (a) {
+        //A capture group is allowed on a match, and will return the enum
+        //value matched. If the payload types of both cases are the same,
+        //they can be put into the same switch prong
+        Item.a, Item.e => |item| item,
+
+        //A reference to the matched value can be obtained using the * syntax
+        Item.c => |*item| blk: {
+            item.*.x += 1;
+            break :blk 6;
+        },
+
+        //No else is required ifthe types cases was exhaustively handled
+        Item.d => 8,
+    };
+
+    try expect(b == 6);
+    try expect(a.c.x == 2);
+}
+
+const Color1 = enum {
+    auto,
+    off,
+    on,
+};
+
+test "enum literals with switch" {
+    const color = Color1.off;
+    const result = switch (color) {
+        .auto => false,
+        .on => false,
+        .off => true,
+    };
+    try expect(result);
+}
