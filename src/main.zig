72af51497c6c5e828799ff834684825800a31a64
Brad Laurie <blaurie@users.noreply.github.com>
2023-05-15 23:05:45 -0600

    some more coercion

--- src/main.zig
+++ src/main.zig
@@ -2729,4 +2729,71 @@ test "optional compile time reflection" {
 //integers coerce to wider integers
 //floats coerce to wider floats
 
-//Cosercion of Slices, Arrays and Pointers
+//Coercion of Slices, Arrays and Pointers ------------------------
+
+//You can assign constant pointers to arrays to a slice with const modifier
+//on the element type. Useful for String literals
+test "*const [N]T to []const T" {
+    var x1: []const u8 = "hello";
+    var x2: []const u8 = &[5]u8{ 'h', 'e', 'l', 'l', 111 };
+    try expect(std.mem.eql(u8, x1, x2));
+
+    var y: []const f32 = &[2]f32{ 1.2, 3.4 };
+    try expect(y[0] == 1.2);
+}
+
+//it works when the destination type is an error union
+test "*const [N]T to E![]const T" {
+    var x1: anyerror![]const u8 = "hello";
+    var x2: anyerror![]const u8 = &[5]u8{ 'h', 'e', 'l', 'l', 111 };
+    try expect(std.mem.eql(u8, try x1, try x2));
+
+    var y: anyerror![]const f32 = &[2]f32{ 1.2, 3.4 };
+    try expect((try y)[0] == 1.2);
+}
+
+//it works when the destination type is an optional
+test "*const [N]T to E![]const T" {
+    var x1: ?[]const u8 = "hello";
+    var x2: ?[]const u8 = &[5]u8{ 'h', 'e', 'l', 'l', 111 };
+    try expect(std.mem.eql(u8, x1.?, x2.?));
+
+    var y: ?[]const f32 = &[2]f32{ 1.2, 3.4 };
+    try expect(y.?[0] == 1.2);
+}
+
+//in this cast, the array length becomes the slice length
+test "*[N]T to []T" {
+    var buf: [5]u8 = "hello".*;
+    const x: []u8 = &buf;
+    try expect(std.mem.eql(u8, x, "hello"));
+
+    const buf2 = [2]f32{ 1.2, 3.4 };
+    const x2: []const f32 = &buf2;
+    try expect(std.mem.eql(f32, x2, &[2]f32{ 1.2, 3.4 }));
+}
+
+//Single-item pointers to arrays can be coerced in to many-item pointers
+test "*[N]T to [*]T" {
+    var buf: [5]u8 = "hello".*;
+    const x: [*]u8 = &buf;
+    try expect(x[4] == 'o');
+    //x[5] would be an uncaught out of bounds pointer dereference!
+}
+
+//this works when the destination is optional
+test "*T to ?[*]T" {
+    const buf: [5]u8 = "hello".*;
+    const x: ?[*]u8 = &buf;
+    try expect(x.?[4] == 'o');
+}
+
+//single-item pointers can be cast to the len-1 single-item arrays
+test "*T to *[1]T" {
+    var x: i32 = 1234;
+    const y: *[1]i32 = &x;
+    const z: [*]i32 = y;
+    try expect(z[0] == 1234);
+}
+
+//Optional coercion
